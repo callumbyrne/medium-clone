2f70a035f46f37c348e777c9165a6bbab84a8afd
Callum Byrne <callumjbyrne97@gmail.com>
2022-03-17 22:50:07 +1100

    inital commit

---
+++ pages/post/[slug].tsx
@@ -0,0 +1,99 @@
+import { GetStaticProps } from 'next'
+import Header from '../../components/Header'
+import { sanityClient, urlFor } from '../../sanity'
+import { Post } from '../../typings'
+import PortableText from 'react-portable-text'
+
+interface Props {
+  post: Post
+}
+
+const Post = ({ post }: Props) => {
+  console.log(post)
+  return (
+    <main>
+      <Header />
+
+      <img
+        className="h-40 w-full object-cover"
+        src={urlFor(post.mainImage).url()}
+        alt=""
+      />
+
+      <article className="mx-auto max-w-3xl p-5">
+        <h1 className="mt-10 mb-3 text-3xl">{post.title}</h1>
+        <h2 className="mb-2 text-xl font-light text-gray-500">
+          {post.description}
+        </h2>
+        <div>
+          <img
+            className="h-10 w-10 rounded-full"
+            src={urlFor(post.author.image).url()}
+            alt=""
+          />
+          <p className="text-sm font-extralight">
+            Blog post by{' '}
+            <span className="text-green-600">{post.author.name}</span> -
+            Published at {new Date(post._createdAt).toLocaleString()}
+          </p>
+        </div>
+      </article>
+    </main>
+  )
+}
+
+export default Post
+
+export const getStaticPaths = async () => {
+  const query = `*[_type == "post"]{
+      _id,
+      slug {
+         current
+      }
+    }`
+
+  const posts = await sanityClient.fetch(query)
+
+  const paths = posts.map((post: Post) => ({
+    params: {
+      slug: post.slug.current,
+    },
+  }))
+
+  return {
+    paths,
+    fallback: 'blocking',
+  }
+}
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const query = `*[_type == "post" && slug.current == $slug][0]{
+   _id,
+   _createdAt,
+   title,
+   author-> {
+     name,
+     image
+   },
+   description,
+   mainImage,
+   slug,
+   body
+ }`
+
+  const post = await sanityClient.fetch(query, {
+    slug: params?.slug,
+  })
+
+  if (!post) {
+    return {
+      notFound: true,
+    }
+  }
+
+  return {
+    props: {
+      post,
+    },
+  }
+}
